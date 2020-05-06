6389da7098bde7608211d6c32ae72ebbb53b78ff
rogeraabbccdd <rogeraabbccdd@gmail.com>
2020-05-06 17:13:51 +0800

    Fix SQL query encoding

--- web/libs/sql.php
+++ web/libs/sql.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$pdo = new PDO("mysql:host=$db_host;dbname=$db_name",$db_user,$db_password);
+		$pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8",$db_user,$db_password);
 		$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	}
 	catch(PDOException $e){
@@ -8,5 +8,4 @@
 	}
 
 	date_default_timezone_get($timezone);
-	// session_start();
 ?>
